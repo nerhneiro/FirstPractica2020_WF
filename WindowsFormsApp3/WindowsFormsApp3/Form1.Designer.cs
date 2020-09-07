d1695a9db70eb8e3d667111d32dd2a8e61a8a4e5
Irene Skvortsova <63853354+nerhneiro@users.noreply.github.com>
2020-09-07 21:37:52 +0300

    virsion 2

---
+++ WindowsFormsApp3/WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LightSalmon;
+            this.button1.Location = new System.Drawing.Point(146, 278);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(166, 91);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Нарисовать";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Bisque;
+            this.panel1.Location = new System.Drawing.Point(357, -6);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(846, 720);
+            this.panel1.TabIndex = 1;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Orange;
+            this.ClientSize = new System.Drawing.Size(1200, 703);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Практическое задание 1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ WindowsFormsApp3/WindowsFormsApp3/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            panel1.Location = new Point(ClientSize.Width / 2, 0);
+            
+        }
+        bool paint = false;
+        bool exist = false;
+        int x1, x2, x3, y1, y2, y3;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            panel1.Width = ClientSize.Width / 2;
+            panel1.Height = ClientSize.Height;
+        }
+
+        float x23_diff, y1_diff;
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            int center_x = panel1.Width / 2;
+            int center_y = panel1.Height / 2;
+            int p1_x = 0, p1_y = 0, p2_x = 0, p2_y = 0, p3_x = 0, p3_y = 0;
+            Console.WriteLine(exist);
+            if (!exist)
+            {
+                Console.WriteLine("first");
+                x23_diff = center_x / 2;
+                y1_diff = center_y / 2;
+                p1_x = center_x;
+                p1_y = (int)(center_y - y1_diff);
+                p2_x = (int)(center_x - x23_diff);
+                p3_x = (int)(center_x + x23_diff);
+                p2_y = center_y;
+                p3_y = center_y;
+                x1 = p1_x;  x2 = p2_x; x3 = p3_x; y1 = p1_y; y2 = p2_y; y3 = p3_y;
+                Console.WriteLine("p1_x = " + x1 + " p1_y = " + y1 + " p2_x = " + x2 + " p2_y = " + y2 + " p3_x = " + x3 + " p3_y = " + y3);
+            }
+            else
+            {
+                Console.WriteLine("next");
+                y1_diff = (float)(y1_diff * 1.1);
+                x23_diff = (float)(x23_diff * 1.1);
+                Console.WriteLine("y1_diff " + y1_diff, " x23_diff " + x23_diff);
+                y1 = (int)((float)center_y - y1_diff);
+                x2 = (int)((float)center_x - x23_diff);
+                x3 = (int)((float)center_x + x23_diff);
+                Console.WriteLine("p1_x = " + x1 + " p1_y = " + y1 + " p2_x = " + x2 + " p2_y = " + y2 + " p3_x = " + x3 + " p3_y = " + y3);
+            }
+            Graphics g = e.Graphics;
+            SolidBrush brush = new SolidBrush(Color.YellowGreen);
+            Point[] points = { new Point(x1, y1), new Point(x2,y2), new Point(x3, y3) };
+            //if (x1 > 0 || y1 < 0 || x3 < 0) paint = false;
+            panel1.Width = ClientSize.Width / 2;
+            panel1.Height = ClientSize.Height;
+            if (paint)
+            {
+                if (y1 >= 0 && x3 >= 0)
+                {
+                    g.FillPolygon(brush, points);
+                    exist = true;
+                    button1.Text = "увеличить";
+                }
+                else 
+                { 
+                    exist = false;
+                    button1.Text = "начать";
+                }
+                
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            panel1.Width = Width / 2;
+            panel1.Height = ClientSize.Height;
+            paint = true;
+        }
+    }
+}
